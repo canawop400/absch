f71a4ddcde252f106157baa52bd551c385dfac04
canawop400 <114168089+canawop400@users.noreply.github.com>
2022-10-07 09:07:15 -0700

    Adding database and responsiveness
    I changed px to rem and set the root (html) font size relative to the vw

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import sqlite3
 
 
 
@@ -20,3 +21,45 @@ def news():
 @app.route("/contacto/")
 def contact():
 	return render_template("contact.html")
+
+@app.route("/noticias/crear/")
+def create_new():
+	return render_template("create_new.html")
+
+# Database
+
+def get_db():
+	conn = sqlite3.connect("database.db")
+	conn.row_factory = sqlite3.Row
+
+	return conn
+
+
+# API
+
+@app.get("/api/news/")
+def get_news():
+	conn = get_db()
+	rows = conn.execute("SELECT * FROM news").fetchall()
+	conn.close()
+
+	news = {
+		"news": []
+	}
+
+	for item in rows:
+		news["news"].append({"title": item["title"], "content": item["content"]})
+
+	return news
+
+
+@app.post("/api/news/")
+def post_news():
+
+	title = request.form["title"]
+	content = request.form["content"]
+
+	if title == "" or content == "":
+		return ("Error: Ninguno de los campos puede estar vacio", 400)
+	
+	return f"Title: {request.form['title']}, Content: {request.form['content']}"
